415c7a3c083e7c6541a62d9ef7c1a41e4d4e16a4
Hoyeong Heo <hyheo@solidware.io>
2017-05-25 00:57:21 +0900

    Apply bootstrap4

--- about.md
+++ about.md
@@ -1,7 +1,6 @@
 ---
 layout: page
 title: About
-skipAdvertisement: true
 ---
 
 <p class="message">
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll serve --port 8282
+RUBYOPT="-KU -E utf-8:utf-8" bundle exec jekyll serve --port 8282
